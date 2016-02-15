983d668659f319c419110ebe76e7b91e8d028da4
perlnoob <turboclipz@msn.com>
2016-02-15 15:32:20 -0800

    Create pinned.pl

---
+++ pinned.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+
+## Add your preference file for chrome, add the URL's you want in the @urls array (you can add more than just 2), close chrome and run... Enjoy
+
+
+my $pref_file = 'chrome_preferences_file';
+
+my @urls = ('URL1','URL2');
+
+my $insert =',"pinned_tabs":[';
+
+for my $i (@urls) {
+	if(  \$i == \$urls[-1]  ) {
+		print "last itteration \n";
+		$insert .= '{"url":"' . $i . '"}],';
+	} else {
+		$insert .= '{"url":"' . $i . '"},';
+	}
+}
+
+my $ignore = ',"pinned_tabs":\[\{"url":';
+
+my $find = ',\"pinned_tabs\":\[],';
+
+open (my $fh, "<", "$pref_file") or die "Could not open file '$pref_file' $!";
+
+my $file = <$fh>; 
+
+close $fh;
+
+if ($file =~ m/$ignore/) {
+	die "Pinned tab entry already exists";
+}
+
+open ($fh, ">", "$pref_file") or die "Could not open file '$pref_file' $!";
+
+$file =~ s/$find/$insert/;
+
+print $fh "$file";
+
+close $fh;
+
+print "Pinned tab entry modified- \n Found: $find \n Replaced with: $insert";
